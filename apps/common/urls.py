10617ef94bd8972ec0284cb3e761ab0b319608ce
hossainimamrony <hossainimamrony71@gmail.com>
2026-05-07 16:23:51 +0600

    dashboard desing improved

--- apps/common/urls.py
+++ apps/common/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import DashboardView
+from .views import ApiDocsView, DashboardView
 
 app_name = "common"
 
 urlpatterns = [
     path("", DashboardView.as_view(), name="dashboard"),
+    path("api-docs/", ApiDocsView.as_view(), name="api-docs"),
 ]
--- apps/common/views.py
+++ apps/common/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class DashboardView(TemplateView):
     template_name = "common/dashboard.html"
+
+
+class ApiDocsView(TemplateView):
+    template_name = "common/api_docs.html"
